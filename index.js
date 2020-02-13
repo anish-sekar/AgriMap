d5e6d1767155f1a7e1b8184287ec42bdd60d4a4f
Anish <anish.sekar94@gmail.com>
2020-02-12 22:31:47 -0800

    Bug fixes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
+import Feature from 'ol/Feature'
+import ol from 'ol'
 import { Draw, Modify, Select, Snap } from 'ol/interaction';
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
 import { OSM, Vector as VectorSource } from 'ol/source';
@@ -12,7 +14,7 @@ var raster = new TileLayer({
     source: new OSM()
 });
 
-vs = new VectorSource()
+var vs = new VectorSource()
 var vector = new VectorLayer({
     source: vs
 });
@@ -26,137 +28,6 @@ var map = new Map({
         zoom: 4
     })
 });
-
-
-veccy = {
-  "disposed_": false,
-  "pendingRemovals_": {},
-  "dispatching_": {},
-  "listeners_": {
-    "change:geometry": [
-      null
-    ],
-    "change": [
-      null,
-      null
-    ],
-    "propertychange": [
-      null
-    ]
-  },
-  "revision_": 24,
-  "ol_uid": "74",
-  "values_": {
-    "geometry": {
-      "disposed_": false,
-      "pendingRemovals_": {},
-      "dispatching_": {},
-      "listeners_": {
-        "change": [
-          null
-        ]
-      },
-      "revision_": 23,
-      "ol_uid": "73",
-      "values_": {},
-      "extent_": [
-        -15233873.822880812,
-        3797195.7345643155,
-        -13414061.053467335,
-        6145341.24348493
-      ],
-      "extentRevision_": 23,
-      "simplifiedGeometryMaxMinSquaredTolerance": 23931368.624409948,
-      "simplifiedGeometryRevision": 23,
-      "layout": "XY",
-      "stride": 2,
-      "flatCoordinates": [
-        -15233873.822880812,
-        3797195.7345643155,
-        -13414061.053467335,
-        4716886.058891556,
-        -13560820.147774873,
-        6145341.24348493,
-        -15233873.822880812,
-        3797195.7345643155
-      ],
-      "ends_": [
-        8
-      ],
-      "flatInteriorPointRevision_": -1,
-      "flatInteriorPoint_": null,
-      "maxDelta_": -1,
-      "maxDeltaRevision_": -1,
-      "orientedRevision_": 23,
-      "orientedFlatCoordinates_": [
-        -15233873.822880812,
-        3797195.7345643155,
-        -13560820.147774873,
-        6145341.24348493,
-        -13414061.053467335,
-        4716886.058891556,
-        -15233873.822880812,
-        3797195.7345643155
-      ]
-    }
-  },
-  "geometryName_": "geometry",
-  "geometryChangeKey_": {
-    "target": {
-      "disposed_": false,
-      "pendingRemovals_": {},
-      "dispatching_": {},
-      "listeners_": {
-        "change": [
-          null
-        ]
-      },
-      "revision_": 23,
-      "ol_uid": "73",
-      "values_": {},
-      "extent_": [
-        -15233873.822880812,
-        3797195.7345643155,
-        -13414061.053467335,
-        6145341.24348493
-      ],
-      "extentRevision_": 23,
-      "simplifiedGeometryMaxMinSquaredTolerance": 23931368.624409948,
-      "simplifiedGeometryRevision": 23,
-      "layout": "XY",
-      "stride": 2,
-      "flatCoordinates": [
-        -15233873.822880812,
-        3797195.7345643155,
-        -13414061.053467335,
-        4716886.058891556,
-        -13560820.147774873,
-        6145341.24348493,
-        -15233873.822880812,
-        3797195.7345643155
-      ],
-      "ends_": [
-        8
-      ],
-      "flatInteriorPointRevision_": -1,
-      "flatInteriorPoint_": null,
-      "maxDelta_": -1,
-      "maxDeltaRevision_": -1,
-      "orientedRevision_": 23,
-      "orientedFlatCoordinates_": [
-        -15233873.822880812,
-        3797195.7345643155,
-        -13560820.147774873,
-        6145341.24348493,
-        -13414061.053467335,
-        4716886.058891556,
-        -15233873.822880812,
-        3797195.7345643155
-      ]
-    },
-    "type": "change"
-  }
-}
 
 
 
@@ -206,12 +77,29 @@ optionsForm.onchange = function(e) {
     }
 };
 
+var obj = {"disposed_":false,"pendingRemovals_":{},"dispatching_":{},"listeners_":{"change:geometry":[null],"change":[null,null],"propertychange":[null]},"revision_":24,"ol_uid":"74","values_":{"geometry":{"disposed_":false,"pendingRemovals_":{},"dispatching_":{},"listeners_":{"change":[null]},"revision_":23,"ol_uid":"73","values_":{},"extent_":[-15233873.822880812,3797195.7345643155,-13414061.053467335,6145341.24348493],"extentRevision_":23,"simplifiedGeometryMaxMinSquaredTolerance":23931368.624409948,"simplifiedGeometryRevision":23,"layout":"XY","stride":2,"flatCoordinates":[-15233873.822880812,3797195.7345643155,-13414061.053467335,4716886.058891556,-13560820.147774873,6145341.24348493,-15233873.822880812,3797195.7345643155],"ends_":[8],"flatInteriorPointRevision_":-1,"flatInteriorPoint_":null,"maxDelta_":-1,"maxDeltaRevision_":-1,"orientedRevision_":23,"orientedFlatCoordinates_":[-15233873.822880812,3797195.7345643155,-13560820.147774873,6145341.24348493,-13414061.053467335,4716886.058891556,-15233873.822880812,3797195.7345643155]}},"geometryName_":"geometry","geometryChangeKey_":{"target":{"disposed_":false,"pendingRemovals_":{},"dispatching_":{},"listeners_":{"change":[null]},"revision_":23,"ol_uid":"73","values_":{},"extent_":[-15233873.822880812,3797195.7345643155,-13414061.053467335,6145341.24348493],"extentRevision_":23,"simplifiedGeometryMaxMinSquaredTolerance":23931368.624409948,"simplifiedGeometryRevision":23,"layout":"XY","stride":2,"flatCoordinates":[-15233873.822880812,3797195.7345643155,-13414061.053467335,4716886.058891556,-13560820.147774873,6145341.24348493,-15233873.822880812,3797195.7345643155],"ends_":[8],"flatInteriorPointRevision_":-1,"flatInteriorPoint_":null,"maxDelta_":-1,"maxDeltaRevision_":-1,"orientedRevision_":23,"orientedFlatCoordinates_":[-15233873.822880812,3797195.7345643155,-13560820.147774873,6145341.24348493,-13414061.053467335,4716886.058891556,-15233873.822880812,3797195.7345643155]},"type":"change"}}
+
+
+var marker = new Feature({
+    geometry: new ol.geometry.point(ol.proj.transform([16.9071388, 52.4901917], 'EPSG:4326', 'EPSG:3857')),
+});
+
+var markers = new ol.source.Vector({
+    features: [marker]
+});
+
+var markerVectorLayer = new ol.layer.Vector({
+    source: markers,
+});
+map.addLayer(markerVectorLayer);
+
 
 button.addEventListener("click", function(e) {
-
    select.getFeatures().forEach(function(each) {
-        
-        alert(JSON.stringify(each))
+    
+
+    alert(JSON.stringify(each))
+
 
       });
 
