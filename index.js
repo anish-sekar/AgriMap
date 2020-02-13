cf2d6de3f6c58912409b34acc3d01cffb291fe83
Anish <anish.sekar94@gmail.com>
2020-02-12 21:19:10 -0800

    Draw and edit features added

--- index.js
+++ index.js
@@ -1,173 +1,218 @@
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import {Draw, Modify, Select, Snap} from 'ol/interaction';
-import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
-import {OSM, Vector as VectorSource} from 'ol/source';
-import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
-import {click, pointerMove, altKeyOnly} from 'ol/events/condition';
+import { Draw, Modify, Select, Snap } from 'ol/interaction';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import { OSM, Vector as VectorSource } from 'ol/source';
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
+import { click, pointerMove, altKeyOnly } from 'ol/events/condition';
 
+// Loading Open street maps renderings
 var raster = new TileLayer({
-  source: new OSM()
+    source: new OSM()
 });
 
+vs = new VectorSource()
 var vector = new VectorLayer({
-  source: new VectorSource(),
-  style: new Style({
-    fill: new Fill({
-      color: 'rgba(255, 255, 255, 0.2)'
-    }),
-    stroke: new Stroke({
-      color: '#ffcc33',
-      width: 2
-    }),
-    image: new CircleStyle({
-      radius: 7,
-      fill: new Fill({
-        color: '#ffcc33'
-      })
-    })
-  })
+    source: vs
 });
+
 
 var map = new Map({
-  layers: [raster, vector],
-  target: 'map',
-  view: new View({
-    center: [-11000000, 4600000],
-    zoom: 4
-  })
+    layers: [raster, vector],
+    target: 'map',
+    view: new View({
+        center: [-11000000, 4600000],
+        zoom: 4
+    })
 });
 
-var ExampleModify = {
-  init: function() {
-    this.select = new Select();
-    map.addInteraction(this.select);
 
-    this.modify = new Modify({
-      features: this.select.getFeatures()
-    });
-    map.addInteraction(this.modify);
-
-    this.setEvents();
+veccy = {
+  "disposed_": false,
+  "pendingRemovals_": {},
+  "dispatching_": {},
+  "listeners_": {
+    "change:geometry": [
+      null
+    ],
+    "change": [
+      null,
+      null
+    ],
+    "propertychange": [
+      null
+    ]
   },
-  setEvents: function() {
-    var selectedFeatures = this.select.getFeatures();
-
-    this.select.on('change:active', function() {
-      selectedFeatures.forEach(function(each) {
-        selectedFeatures.remove(each);
-      });
-    });
+  "revision_": 24,
+  "ol_uid": "74",
+  "values_": {
+    "geometry": {
+      "disposed_": false,
+      "pendingRemovals_": {},
+      "dispatching_": {},
+      "listeners_": {
+        "change": [
+          null
+        ]
+      },
+      "revision_": 23,
+      "ol_uid": "73",
+      "values_": {},
+      "extent_": [
+        -15233873.822880812,
+        3797195.7345643155,
+        -13414061.053467335,
+        6145341.24348493
+      ],
+      "extentRevision_": 23,
+      "simplifiedGeometryMaxMinSquaredTolerance": 23931368.624409948,
+      "simplifiedGeometryRevision": 23,
+      "layout": "XY",
+      "stride": 2,
+      "flatCoordinates": [
+        -15233873.822880812,
+        3797195.7345643155,
+        -13414061.053467335,
+        4716886.058891556,
+        -13560820.147774873,
+        6145341.24348493,
+        -15233873.822880812,
+        3797195.7345643155
+      ],
+      "ends_": [
+        8
+      ],
+      "flatInteriorPointRevision_": -1,
+      "flatInteriorPoint_": null,
+      "maxDelta_": -1,
+      "maxDeltaRevision_": -1,
+      "orientedRevision_": 23,
+      "orientedFlatCoordinates_": [
+        -15233873.822880812,
+        3797195.7345643155,
+        -13560820.147774873,
+        6145341.24348493,
+        -13414061.053467335,
+        4716886.058891556,
+        -15233873.822880812,
+        3797195.7345643155
+      ]
+    }
   },
-  setActive: function(active) {
-    this.select.setActive(active);
-    this.modify.setActive(active);
-  },
-
-  getSelected: function(){
-    this.select.getFeatures().forEach(function(each) {
-        console.log(each.getId());
-        alert(each.getId());
-      });
+  "geometryName_": "geometry",
+  "geometryChangeKey_": {
+    "target": {
+      "disposed_": false,
+      "pendingRemovals_": {},
+      "dispatching_": {},
+      "listeners_": {
+        "change": [
+          null
+        ]
+      },
+      "revision_": 23,
+      "ol_uid": "73",
+      "values_": {},
+      "extent_": [
+        -15233873.822880812,
+        3797195.7345643155,
+        -13414061.053467335,
+        6145341.24348493
+      ],
+      "extentRevision_": 23,
+      "simplifiedGeometryMaxMinSquaredTolerance": 23931368.624409948,
+      "simplifiedGeometryRevision": 23,
+      "layout": "XY",
+      "stride": 2,
+      "flatCoordinates": [
+        -15233873.822880812,
+        3797195.7345643155,
+        -13414061.053467335,
+        4716886.058891556,
+        -13560820.147774873,
+        6145341.24348493,
+        -15233873.822880812,
+        3797195.7345643155
+      ],
+      "ends_": [
+        8
+      ],
+      "flatInteriorPointRevision_": -1,
+      "flatInteriorPoint_": null,
+      "maxDelta_": -1,
+      "maxDeltaRevision_": -1,
+      "orientedRevision_": 23,
+      "orientedFlatCoordinates_": [
+        -15233873.822880812,
+        3797195.7345643155,
+        -13560820.147774873,
+        6145341.24348493,
+        -13414061.053467335,
+        4716886.058891556,
+        -15233873.822880812,
+        3797195.7345643155
+      ]
+    },
+    "type": "change"
   }
-};
+}
 
-var selectClick = new Select({
-  condition: click
-});
 
-ExampleModify.init();
 
 var optionsForm = document.getElementById('options-form');
 var button = document.getElementById('button');
 
-var ExampleDraw = {
-  init: function() {
-    map.addInteraction(this.Point);
-    this.Point.setActive(false);
-    map.addInteraction(this.LineString);
-    this.LineString.setActive(false);
-    map.addInteraction(this.Polygon);
-    this.Polygon.setActive(false);
-    map.addInteraction(this.Circle);
-    this.Circle.setActive(false);
-  },
-  Point: new Draw({
-    source: vector.getSource(),
-    type: 'Point'
-  }),
-  LineString: new Draw({
-    source: vector.getSource(),
-    type: 'LineString'
-  }),
-  Polygon: new Draw({
-    source: vector.getSource(),
-    type: 'Polygon'
-  }),
-  Circle: new Draw({
-    source: vector.getSource(),
-    type: 'Circle'
-  }),
-  getActive: function() {
-    return this.activeType ? this[this.activeType].getActive() : false;
-  },
-  setActive: function(active) {
-    var type = optionsForm.elements['draw-type'].value;
-    if (active) {
-      this.activeType && this[this.activeType].setActive(false);
-      this[type].setActive(true);
-      this.activeType = type;
-    } else {
-      this.activeType && this[this.activeType].setActive(false);
-      this.activeType = null;
-    }
-  }
-};
-ExampleDraw.init();
+var poly;
+var select;
+    var snap = new Snap({
+    source: vector.getSource()
+    });
 
-
-/**
- * Let user change the geometry type.
- * @param {Event} e Change event.
- */
 optionsForm.onchange = function(e) {
-  var type = e.target.getAttribute('name');
-  var value = e.target.value;
-  if (type == 'draw-type') {
-    ExampleDraw.getActive() && ExampleDraw.setActive(true);
-  } else if (type == 'interaction') {
-    if (value == 'modify') {
-      ExampleDraw.setActive(false);
-      ExampleModify.setActive(true);
-    } else if (value == 'draw') {
-      ExampleDraw.setActive(true);
-      ExampleModify.setActive(false);
-    }
-    else if(value == 'none'){
+    var type = e.target.getAttribute('name');
+    var value = e.target.value;
+    if (type == 'interaction') {
+        if (value == 'modify') {
+           
+           map.removeInteraction(poly);
+            select = new Select();
+            map.addInteraction(select);
 
-      ExampleDraw.setActive(false);
-      ExampleModify.setActive(false);
-      map.addInteraction(selectClick);
+            var modify = new Modify({
+              features: select.getFeatures()
+              });
 
+            map.addInteraction(modify);
+
+
+        } else if (value == 'draw') {
+
+             poly = new Draw({
+                source: vector.getSource(),
+                type: 'Polygon'
+            })
+
+            map.addInteraction(poly);
+            snap = new Snap({source: source});
+            map.addInteraction(snap);
+
+        } else if (value == 'None') {
+            map.removeInteraction(poly);
+            Select = new Select();
+            map.addInteraction(select);
+
+        }
     }
-  }
 };
+
 
 button.addEventListener("click", function(e) {
-   ExampleModify.getSelected();
+
+   select.getFeatures().forEach(function(each) {
+        
+        alert(JSON.stringify(each))
+
+      });
 
 }, false);
-
-ExampleDraw.setActive(true);
-ExampleModify.setActive(false);
-
-
-
-// The snap interaction must be added after the Modify and Draw interactions
-// in order for its map browser event handlers to be fired first. Its handlers
-// are responsible of doing the snapping.
-var snap = new Snap({
-  source: vector.getSource()
-});
-map.addInteraction(snap);
